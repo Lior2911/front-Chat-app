4a96393a346dfd9da6c2b2fc128dadf6103d0322
lior2911 <aytegev1@gmail.com>
2022-12-26 11:31:52 +0200

    v

--- client/src/components/Auth/LogIn.jsx
+++ client/src/components/Auth/LogIn.jsx
@@ -59,7 +59,7 @@ const LogIn = () => {
         position: "bottom",
       });
       localStorage.setItem("userInfo", JSON.stringify(data));
-      navigate('https://backend-chatup.onrender.com/chats')
+      navigate('/chats')
     
     
     } catch (error) {
